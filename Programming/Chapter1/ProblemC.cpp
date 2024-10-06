53b6b9db49890c09052d7dd6ab4e32fbc121fe5a
lkcqswb <2684779036@qq.com>
2024-10-06 11:03:59 +0800

    10.6

--- Programming/Chapter1/ProblemC.cpp
+++ Programming/Chapter1/ProblemC.cpp
@@ -8,5 +8,6 @@ double func(double x){
 int main(){
     cout<<"Problem C"<<endl;
     Function Function1(func);
-    cout<<"root of x-tan(x) with initial value 4.6: "<<Newton_method(Function1,4.6,1e-3,10).solve()<<endl;
+    cout<<"root of x-tan(x) with initial value 4.5: "<<Newton_method(Function1,4.5,1e-3,10).solve()<<endl;
+    cout<<"root of x-tan(x) with initial value 7.7: "<<Newton_method(Function1,7.7,1e-3,10).solve()<<endl;
 }
--- Programming/Chapter1/ProblemE.cpp
+++ Programming/Chapter1/ProblemE.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<cmath>
+#include <iomanip>
 #include"EquationSolver.hpp"
 double L=10,r=1,V=12.4;
 
@@ -11,7 +12,7 @@ double volumn(double h){
 int main(){
     Function Vol(&volumn);
     cout<<"Problem E"<<endl;
-    cout<<"using bisection method: "<<bisection_method(Vol,0,r,1e-3,1e-2,400).solve()<<endl;
-    cout<<"using Newton method: "<<Newton_method(Vol,0.5*r,1e-2,400).solve()<<endl;
-    cout<<"using secant method: "<<Secant_method(Vol,0,r,1e-3,1e-2,400).solve()<<endl;
+    cout<<"using bisection method: "<<fixed << setprecision(2) <<bisection_method(Vol,0,r,1e-3,1e-4,500).solve()<<endl;
+    cout<<"using Newton method: "<<fixed << setprecision(2)<<Newton_method(Vol,0.5*r,1e-3,400).solve()<<endl;
+    cout<<"using secant method: "<<fixed << setprecision(2)<<Secant_method(Vol,0,r,1e-3,1e-3,400).solve()<<endl;
 }
